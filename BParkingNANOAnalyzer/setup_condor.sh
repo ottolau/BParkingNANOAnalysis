f7edc4c27d3a3287ba8b46a75408297c4e37cfda
KaTung Lau <ka_tung_lau@brown.edu>
2020-01-29 16:23:12 +0100

    update

--- BParkingNANOAnalyzer/setup_condor.sh
+++ BParkingNANOAnalyzer/setup_condor.sh
@@ -1,5 +1,2 @@
 ### bash --login
-pip install uproot --upgrade --upgrade-strategy only-if-needed --install-option="--prefix=${MAINDIR}/.local"
-pip install awkward --install-option="--prefix=${MAINDIR}/.local"
-pip install scikit-optimize  --install-option="--prefix=${MAINDIR}/.local"
-pip install uproot_methods  --install-option="--prefix=${MAINDIR}/.local"
+pip install uproot --upgrade --upgrade-strategy only-if-needed --user --install-option="--prefix=${MAINDIR}/.local"
--- BParkingNANOAnalyzer/test/condor/submit_condor_lxplus.py
+++ BParkingNANOAnalyzer/test/condor/submit_condor_lxplus.py
@@ -33,10 +33,10 @@ def write_condor(exe='runjob.sh', arguments = [], files = [],dryRun=True):
     out += 'use_x509userproxy = true\n'
     out += 'x509userproxy = $ENV(X509_USER_PROXY)\n' # for lxplus
     out += 'Arguments = %s\n'%(' '.join(arguments))
-    #out += '+JobFlavour = "espresso"\n'
+    out += '+JobFlavour = "espresso"\n'
     #out += '+JobFlavour = "longlunch"\n'
     #out += '+JobFlavour = "workday"\n'
-    out += '+MaxRuntime = 36000\n'
+    #out += '+MaxRuntime = 36000\n'
     out += 'on_exit_remove = (ExitBySignal == False) && (ExitCode == 0)\n'
     out += 'max_retries = 2\n'
     out += 'requirements = Machine =!= LastRemoteHost\n'
